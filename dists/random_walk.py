bde350750f7566d7229891fff83a14c84173f136
Ethan Duckmanton <ecd@This-degree-is-shite.local>
2026-05-12 10:27:26 +0100

    edited figure

--- dists/random_walk.py
+++ dists/random_walk.py
@@ -5,7 +5,7 @@ def PDF(n,N):
     return 1/(np.sqrt(2*np.pi*N)) * np.exp(-(n**2)/(2*N))
 
 Nspecies = 10000
-N=100000
+N=10000
 
 species = np.zeros(Nspecies)
 
@@ -17,11 +17,16 @@ for i in range(N):
 Fig = plot.figure()
 plot.hist(species, bins=200, density=True, alpha=0.5, color='blue')
 
+mean = np.mean(species)
+std = np.std(species)
+
+plot.text(140,0.003,"Model Mean $\pm$ STD = {0} $\pm$ {1} ,\nMean $\pm$ STD = {2:3.2f} $\pm$ {3:3.2f}".format(0, np.sqrt(N), mean, std))
+
 n = np.linspace(np.min(species), np.max(species), 100)
 y = PDF(n,N)
 plot.plot(n,y,"k--",label = "Random Walk PDF")
 
-plot.title("Histogram of Simulated Random Walk, {0} species, N = {1}".format(Nspecies,N))
+plot.title("Histogram of Simulated Random Walk, {0} tests, N = {1}".format(Nspecies,N))
 plot.xlabel('n')
 plot.ylabel('Density')
 plot.legend()
